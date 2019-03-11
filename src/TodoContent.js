2090699829db3eae6786f3af80927c7c50d441ae
HYEON(Mac) <antaehyeon@live.co.kr>
2019-03-12 03:54:33 +0900

    Feat: update TodoContent Style

--- src/TodoContent.js
+++ src/TodoContent.js
@@ -1,11 +1,74 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import Toast from "react-native-simple-toast";
 
 const TodoContent = () => {
   return (
-    <View style={styles.todoContentContainer}>
-      <Text style={styles.todoContentText}>Learning React Native Hooks</Text>
-    </View>
+    <>
+      <View style={styles.todoContentContainer}>
+        <View style={styles.horizontalCenter}>
+          <Icon.Button
+            name="circle"
+            backgroundColor="transparent"
+            size={20}
+            color="black"
+            style={styles.iconMargin}
+          />
+          <Text style={styles.todoContentText}>
+            Learning React Native Hooks
+          </Text>
+        </View>
+        <View style={styles.horizontalCenter}>
+          <Icon.Button
+            name="edit"
+            backgroundColor="transparent"
+            size={20}
+            color="black"
+            onPress={() => Toast.show("MODIFY")}
+          />
+          <Icon.Button
+            name="trash-2"
+            backgroundColor="transparent"
+            size={20}
+            color="black"
+            onPress={() => Toast.show("DELETE")}
+          />
+        </View>
+      </View>
+
+      <View style={styles.todoContentContainer}>
+        <View style={styles.horizontalCenter}>
+          <Icon.Button
+            name="check-circle"
+            backgroundColor="transparent"
+            style={{ margin: 0, padding: 0 }}
+            size={20}
+            color="black"
+            style={styles.iconMargin}
+          />
+          <Text style={[styles.todoContentText, styles.textLineThrough]}>
+            Learning React Native Hooks
+          </Text>
+        </View>
+        <View style={styles.horizontalCenter}>
+          <Icon.Button
+            name="edit"
+            backgroundColor="transparent"
+            size={20}
+            color="black"
+            onPress={() => Toast.show("MODIFY")}
+          />
+          <Icon.Button
+            name="trash-2"
+            backgroundColor="transparent"
+            size={20}
+            color="black"
+            onPress={() => Toast.show("DELETE")}
+          />
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -13,12 +76,25 @@ const styles = StyleSheet.create({
   todoContentContainer: {
     width: "100%",
     height: 40,
-    backgroundColor: "blue",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "space-between",
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "grey"
+  },
+  iconMargin: {
+    marginLeft: 10
   },
   todoContentText: {
-    color: "white"
+    color: "black",
+    marginLeft: 10
+  },
+  textLineThrough: {
+    textDecorationLine: "line-through"
+  },
+  horizontalCenter: {
+    flexDirection: "row",
+    alignItems: "center"
   }
 });
 
